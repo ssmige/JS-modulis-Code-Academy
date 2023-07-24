7832f3902459c743a3cd9cc4bc391371d1967ef0
Migle Sniureviciute <ssmige@gmail.com>
2023-07-24 22:54:48 +0300

    JS 1 paskaita

---
+++ JS-1 paskaita/index.js
@@ -0,0 +1,28 @@
+// console.log(123);
+
+const name = "Migle"
+
+let age = 26;
+console.log(age);
+age = 30;
+console.log(age);
+
+console.log(name);
+
+const sum = 10+15;
+console.log(sum);
+
+const sub = 10-7;
+console.log(sub);
+
+const mult =3*8;
+console.log(mult);
+
+const div = 6/2;
+console.log(div);
+
+const sum1 = "labas"+"Ate";
+console.log(sum1);
+
+const sub3 = age+7;
+console.log(sub3);
---
+++ JS-1 paskaita/uzduotis1/index.js
@@ -0,0 +1,24 @@
+// .............uzduotis 1
+
+// console.log("Hello World!");
+// alert("I love JavaScript!");
+// alert("Ready to learn more?");
+// const number =prompt("Ivesk skaicius nuo 1 iki 10");
+// console.log(number);
+
+
+// .............uzduotis 2
+
+// const name = prompt ("Iveskite savo varda");
+// const birthDay = "Geguzes 12";
+
+// const fullText = name + " yra gimes " + birthDay;
+// console.log(fullText);
+
+//............uzduotis 3
+
+const bazinisUzmokestis=prompt("Iveskite bazini uzmokesti");
+const workingPeople = 1761463;
+const budget = 8487300000;
+const precent = (workingPeople * bazinisUzmokestis*100/ budget);
+document.write(precent);
