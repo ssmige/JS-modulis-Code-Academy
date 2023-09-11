bebe968d91bec64eaee6fbfd4ffaaa089a76e32a
Migle Sniureviciute <ssmige@gmail.com>
2023-09-11 18:55:26 +0300

    1 uzduotis

--- js-exam/Task 1/script.js
+++ js-exam/Task 1/script.js
@@ -8,3 +8,27 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const inputKg = document.querySelector("#search");
+const outputDiv = document.querySelector("#output");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", convert);
+function convert(e) {
+  e.preventDefault();
+  let kg = inputKg.value;
+
+  let svarai = kg * 2.2046;
+  const outputlb = document.createElement("h3");
+  outputlb.textContent = `${svarai} lb `;
+
+  let gramai = (kg * kg) / 0.001;
+  const outputGr = document.createElement("h3");
+  outputGr.textContent = `${gramai} g `;
+
+  let uncijos = kg * 35.274;
+  const outputOz = document.createElement("h3");
+  outputOz.textContent = `${uncijos} lb `;
+
+  outputDiv.append(outputlb, outputGr, outputOz);
+}
